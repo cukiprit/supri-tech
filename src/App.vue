6f2d08b68b79e7e74fd29e288e6a583696ce7b65
Andhika Dwiky Pratama <andhikadwikyp@gmail.com>
2024-05-01 16:43:08 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="container mx-auto flex flex-col items-center justify-center">
+    <div class="flex flex-col justify-center">
+      <div class="mt-4">
+        <img class="max-w-full mx-auto" src="/wip.svg" alt="" width="500px" />
+      </div>
+      <div class="mt-4">
+        <h1 class="text-3xl font-bold text-center">Coming Soon</h1>
+      </div>
+    </div>
+  </main>
+</template>
