d4752d1fa13357c4bee1f1277ce32975ce81fba5
Hendriansyah <hendriansyah170522@gmail.com>
2024-05-23 14:44:41 +0700

    Layanan

--- src/components/CardMissionCompany.vue
+++ src/components/CardMissionCompany.vue
@@ -1,19 +1,18 @@
 <script>
 export default {
-  props: [
-    'name'
-  ]
+  props: ['name'],
 };
 </script>
 <template>
   <div id="card-mission-company">
-    <a href="#"
-      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-
+    <a
+      href="#"
+      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ name }}</h5>
-      <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of
-        2021 so far, in reverse chronological order.</p>
+      <p class="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
     </a>
-
   </div>
 </template>
---
+++ src/components/CardPricePelayanan.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: ['name', 'price', 'label', 'fitur'],
+};
+</script>
+<template>
+  <div id="card-price" class="">
+    <a href="#" class="block max-w-lg p-6 text-center shadow-2xl">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight dark:text-white">{{ name }}</h5>
+      <p class="font-normal mb-2">Mulai dari Harga</p>
+      <div class="absolute flex flex-row">
+        <h4 class="text-2xl font-bold">
+          {{ price }}
+        </h4>
+        <p class="text-sm font-semibold">\{{ label }}</p>
+      </div>
+      <h4 class="pt-10">Fitur yang kami tawarkan :</h4>
+      <div class="flex justify-center items-center">
+        <ul>
+          <li>{{ fitur }}</li>
+        </ul>
+      </div>
+    </a>
+  </div>
+</template>
